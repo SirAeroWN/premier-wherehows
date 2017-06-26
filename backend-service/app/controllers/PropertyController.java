5ae7cae5d581c9e2bdceb958233dda009d016c1f
Will Norvelle <will_norvelle@premierinc.com>
2017-06-26 10:44:32 -0400

    should include default properties on build

---
+++ backend-service/app/controllers/PropertyController.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2015 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.List;
+import models.daos.DatasetDao;
+import models.daos.UserDao;
+import utils.Urn;
+import org.springframework.dao.EmptyResultDataAccessException;
+import play.Logger;
+import play.libs.Json;
+import play.mvc.BodyParser;
+import play.mvc.Controller;
+import play.mvc.Result;
